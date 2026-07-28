6fc27b23f049ba71c2082a3d6709ba04412ed8c4
Thakshila Bandara <e21050@eng.pdn.ac.lk>
2026-07-28 19:46:01 +0530

    chore: setup github pages deployment via actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/me/', // Repository name for GitHub Pages
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
